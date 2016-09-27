f1e9264a96d67b32adb86288407bf16840979d55
levydev <levydev@gmail.com>
2016-09-26 21:15:46 -0400

    add new training materials

--- src/main/java/org/ftcTeam/FTCTeamControllerActivity.java
+++ src/main/java/org/ftcTeam/FTCTeamControllerActivity.java
@@ -3,7 +3,6 @@ package org.ftcTeam;
 import com.qualcomm.robotcore.eventloop.opmode.OpModeRegister;
 
 import org.ftcTeam.opmodes.*;
-import org.ftcTeam.training.*;
 
 import org.firstinspires.ftc.robotcontroller.internal.FtcRobotControllerActivity;
 
@@ -13,9 +12,9 @@ public class FTCTeamControllerActivity extends FtcRobotControllerActivity {
   @Override
   protected OpModeRegister createOpModeRegister() {
 
-      //return new Registrar1();
+      return new Registrar1();
       //return new Registrar2();
-      return new TrainingBeginnerRegistrar();
+
 
 
   }
