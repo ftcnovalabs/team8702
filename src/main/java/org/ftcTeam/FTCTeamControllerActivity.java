9cf654026d5cc44d4a539d1a248085c16d0af90c
daniel.sw.kim@gmail.com <daniel.sw.kim@gmail.com>
2016-10-09 09:53:49 -0400

    Testing

--- src/main/java/org/ftcTeam/FTCTeamControllerActivity.java
+++ src/main/java/org/ftcTeam/FTCTeamControllerActivity.java
@@ -8,6 +8,7 @@ import org.firstinspires.ftc.robotcontroller.internal.FtcRobotControllerActivity
 
 public class FTCTeamControllerActivity extends FtcRobotControllerActivity {
 
+    
 
   @Override
   protected OpModeRegister createOpModeRegister() {
