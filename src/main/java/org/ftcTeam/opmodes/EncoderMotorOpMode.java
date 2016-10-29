d19af0c7bb7f44025b46a8a3538b5109a445e929
Tyler Kim <tyler.ys.kim@gmail.com>
2016-10-28 20:28:00 -0400

    Autonomous INITIALIZED Next Procedure according to the process of creation of a robot for 2016 - 2017 First Vortex Challenge is testing the first attempt of encoder uses.

---
+++ src/main/java/org/ftcTeam/opmodes/EncoderMotorOpMode.java
@@ -0,0 +1,112 @@
+package org.ftcTeam.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.ftcTeam.configurations.Team8702Test;
+import org.ftcbootstrap.ActiveOpMode;
+import org.ftcbootstrap.components.operations.motors.MotorToEncoder;
+import org.ftcbootstrap.components.utils.MotorDirection;
+import org.ftcbootstrap.components.utils.TelemetryUtil;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+/**
+ * Note: This Exercise assumes that you have used your Robot Controller App to "scan" your hardware and
+ * saved the configuration named: "DemoBot" and creating a class by the same name: {@link Team8702Test}.
+ * <p/>
+ * Note:  It is assumed that the proper registry is used for this set of demos. To confirm please
+ * search for "Enter your custom registry here"  in  {@link org.firstinspires.ftc.robotcontroller.internal.FtcRobotControllerActivity;}
+ * <p/>
+ * Summary:
+ * <p/>
+ * Opmode demonstrates running a motor from and encoder
+ */
+
+
+@Autonomous
+public class EncoderMotorOpMode extends ActiveOpMode {
+
+    private Team8702Test robot;
+
+    private MotorToEncoder motorToEncoder;
+    private int step;
+
+    /**
+     * Implement this method to define the code to run when the Init button is pressed on the Driver station.
+     */
+    @Override
+    protected void onInit() {
+
+        //specify configuration name save from scan operation
+        robot = Team8702Test.newConfig(hardwareMap, getTelemetryUtil());
+        motorToEncoder = new MotorToEncoder(  this, robot.motor1);
+        motorToEncoder.setName("motor1");
+
+        getTelemetryUtil().addData("Init", getClass().getSimpleName() + " initialized.");
+        getTelemetryUtil().sendTelemetry();
+
+    }
+
+    @Override
+    protected void onStart() throws InterruptedException  {
+        super.onStart();
+        step = 1;
+    }
+
+    /**
+     * Implement this method to define the code to run when the Start button is pressed on the Driver station.
+     * This method will be called on each hardware cycle just as the loop() method is called for event based Opmodes
+     *  @throws InterruptedException
+     */
+    @Override
+    protected void activeLoop() throws InterruptedException {
+
+
+        getTelemetryUtil().addData("step: " + step , "current");
+
+        boolean targetReached = false;
+
+        switch (step) {
+            case 1:
+
+                //full power , forward for 8880
+                targetReached = motorToEncoder.runToTarget(1, 4000,
+                        MotorDirection.MOTOR_FORWARD, DcMotor.RunMode.RUN_USING_ENCODER);
+                if (targetReached) {
+                    step++;
+                }
+                break;
+
+            case 2:
+                //  1/4 power backward for 1000
+                targetReached = motorToEncoder.runToTarget(0.25, 1000,
+                        MotorDirection.MOTOR_BACKWARD, DcMotor.RunMode.RUN_USING_ENCODER);
+                if (targetReached) {
+                    step++;
+                }
+                break;
+
+            case 99:
+                getTelemetryUtil().addData("step" + step + " Opmode Status", "Robot Stopped.  Kill switch activated");
+                setOperationsCompleted();
+                break;
+
+
+            default:
+                setOperationsCompleted();
+                break;
+
+        }
+
+
+        //send any telemetry that may have been added in the above operations
+        getTelemetryUtil().sendTelemetry();
+
+
+    }
+
+
+
+
+        }
+
