d2cbcfaaedfa0c869e0895864eb55aaa86272f67
Tyler Kim <tyler.ys.kim@gmail.com>
2016-11-13 20:07:31 -0500

    Color Sensor close to completion part 3

--- src/main/java/org/ftcTeam/opmodes/EncoderMotorOpMode.java
+++ src/main/java/org/ftcTeam/opmodes/EncoderMotorOpMode.java
@@ -9,6 +9,8 @@ import org.ftcbootstrap.components.operations.motors.MotorToEncoder;
 import org.ftcbootstrap.components.utils.MotorDirection;
 import org.ftcbootstrap.components.utils.TelemetryUtil;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import org.ftcbootstrap.components.ColorSensorComponent;
+
 
 /**
  * Note: This Exercise assumes that you have used your Robot Controller App to "scan" your hardware and
@@ -31,6 +33,10 @@ public class EncoderMotorOpMode extends ActiveOpMode {
     private MotorToEncoder motorToEncoder;
     private MotorToEncoder motorToEncoderL;
     private int step;
+    String blue = "blue";
+    String red = "red";
+    String green = "green";
+    public ColorSensorComponent colorSensorComponent;
 
     /**
      * Implement this method to define the code to run when the Init button is pressed on the Driver station.
@@ -47,6 +53,8 @@ public class EncoderMotorOpMode extends ActiveOpMode {
 
         getTelemetryUtil().addData("Init", getClass().getSimpleName() + " initialized.");
         getTelemetryUtil().sendTelemetry();
+        colorSensorComponent = new ColorSensorComponent(this, robot.mrColor1, ColorSensorComponent.ColorSensorDevice.MODERN_ROBOTICS_I2C);
+        colorSensorComponent.enableLed(true);
 
     }
 
@@ -63,6 +71,14 @@ public class EncoderMotorOpMode extends ActiveOpMode {
      */
     @Override
     protected void activeLoop() throws InterruptedException {
+        getTelemetryUtil().addData("red", Integer.toString(robot.mrColor1.red()));
+        getTelemetryUtil().addData("blue", Integer.toString(robot.mrColor1.blue()));
+        getTelemetryUtil().addData("green", Integer.toString(robot.mrColor1.green()));
+        getTelemetryUtil().addData("clear", Integer.toString(robot.mrColor1.alpha()));
+        //send any telemetry that may have been added in the above operations
+        getTelemetryUtil().sendTelemetry();
+        getTelemetryUtil().addData("Color",  getColor().toString());
+
 
 
         getTelemetryUtil().addData("step: " + step , "current");
@@ -111,9 +127,25 @@ public class EncoderMotorOpMode extends ActiveOpMode {
 
 
     }
+    public ColorValue getColor() {
+        int Red = colorSensorComponent.getR();
+        int Blue = colorSensorComponent.getB();
+        int Green = colorSensorComponent.getG();
 
+        boolean redboolean = colorSensorComponent.isRed(Red, Blue, Green);
 
-
-
+        if (redboolean) {
+            return ColorValue.RED;
         }
+        boolean blueboolean = colorSensorComponent.isBlue(Red, Blue, Green);
+
+        if(blueboolean) {
+            return ColorValue.BLUE;
+        }
+        return ColorValue.ZILCH;
+
+
+    }
+}
+
 
