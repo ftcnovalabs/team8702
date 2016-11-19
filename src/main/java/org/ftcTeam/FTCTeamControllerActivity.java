ddcb7f0025eeb2a081c93755671d5d80f6d029aa
Daniel Kim <daniel.sw.kim@gmail.com>
2016-11-18 20:39:59 -0500

    Clean up

--- src/main/java/org/ftcTeam/FTCTeamControllerActivity.java
+++ src/main/java/org/ftcTeam/FTCTeamControllerActivity.java
@@ -2,18 +2,19 @@ package org.ftcTeam;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpModeRegister;
 
-import org.ftcTeam.opmodes.registrar1.Registrar1;
+
 
 import org.firstinspires.ftc.robotcontroller.internal.FtcRobotControllerActivity;
-import org.ftcTeam.opmodes.registrar2.Registrar2;
+import org.ftcTeam.opmodes.registrar1.Registrar1;
+
 
 public class FTCTeamControllerActivity extends FtcRobotControllerActivity {
 
   @Override
   protected OpModeRegister createOpModeRegister() {
 
-      //return new Registrar1();
-      return new Registrar2();
+      return new Registrar1();
+
   }
 
 }
